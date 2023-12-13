1e2dbafb8c4bd4bcdd3bd7fa5e8aea81e09fdaea
thangtrn <thangtrn01@gmail.com>
2023-12-13 16:24:47 +0700

    change url

--- src/axios/axiosInstance.ts
+++ src/axios/axiosInstance.ts
@@ -2,8 +2,8 @@
 import axios, { AxiosInstance, AxiosResponse } from 'axios';
 
 const instance: AxiosInstance = axios.create({
-   baseURL: 'https://localhost:7201/api',
-   // baseURL: 'http://ghostrider5024-001-site1.atempurl.com/api',
+   // baseURL: 'https://localhost:7201/api',
+   baseURL: 'http://ghostrider5024-001-site1.atempurl.com/api',
    timeout: 10000,
    headers: {
       'Content-Type': 'application/json',
